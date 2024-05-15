3a95dc9519d2800cbd426112143c8cfe70932b2b
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-05-15 12:41:54 -0700

    updating urls for auth

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -64,6 +64,18 @@ export default function Signin({
     return redirect("/signin?message=Check email to continue sign in process");
   };
 
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+  };
+
   const signInWithGoogle = async () => {
     "use server";
 
@@ -79,7 +91,7 @@ export default function Signin({
     const { error, data } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${origin}/auth/callback`,
+        redirectTo: `${getURL()}auth/callback`,
       },
     });
 
